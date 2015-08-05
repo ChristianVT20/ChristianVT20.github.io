e5f29307337bb9db6f8121efc69593210e28d453
ChristianVT20 <christian.venegas.torres@gmail.com>
2015-08-05 01:06:22 -0600

    Contact added

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,11 @@
 $(window).load(function() {
-    $(".panel").fadeIn(2000, function(){
-    	$(".fb").fadeIn(500, function(){
-    		$(".da").fadeIn(500, function(){
-    			$(".gh").fadeIn(500, function(){});
+	$('body').show(function(){
+		$(".panel").fadeIn(2000, function(){
+    		$(".fb").fadeIn(500, function(){
+	    		$(".da").fadeIn(500, function(){
+	    			$(".gh").fadeIn(500, function(){});
+	    		});
     		});
-    	});
-	})
+		})
+	}); 
 });
